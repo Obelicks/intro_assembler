b851ee05a50b091af8120677f16502f8e9dc9dfa
ecb <bloechrr@gmail.com>
2024-05-09 12:20:14 +0200

    Update Fibo works

--- project/fibonacci.asm
+++ project/fibonacci.asm
@@ -1,9 +1,9 @@
-global _start
-extern printf
+global  _start
+extern  printf
 
 section .data
     message db "Fibonacci Sequence:", 0x0a
-    outFormat db "%d", 0x0a, 0x00
+    outFormat db  "%d", 0x0a, 0x00
 
 section .text
 _start:
@@ -13,36 +13,37 @@ _start:
     call Exit           ; Exit the program
 
 printMessage:
-    mov rax, 1      ; rax: syscall number 1
-    mov rdi, 1      ; rdi: fd 1 for stdout
-    mov rsi,message ; rsi: pointer to message
-    mov rdx, 20     ; rdx: print length of 20 bytes
-    syscall         ; call write syscall to the intro message
+    mov rax, 1           ; rax: syscall number 1
+    mov rdi, 1          ; rdi: fd 1 for stdout
+    mov rsi, message    ; rsi: pointer to message
+    mov rdx, 20          ; rdx: print length of 20 bytes
+    syscall             ; call write syscall to the intro message
     ret
 
 initFib:
-    xor rax, rax    ; initialize rax to 0
-    xor rbx, rbx    ; initialize rbx to 0
-    inc rbx         ; increment rbx to 1
-    ret
-
-loopFib:
-    call printFib
-    add rax, rbx    ; get the next number
-    xchg rax, rbx   ; swap values
-    cmp rbx, 10		; do rbx - 10
-    js loopFib		; jump if result is <0
+    xor rax, rax        ; initialize rax to 0
+    xor rbx, rbx        ; initialize rbx to 0
+    inc rbx             ; increment rbx to 1
     ret
 
 printFib:
-    push rax        ; rax to the top of the stack
-    push rbx        ; rbx to the top of the stack
-    mov rdi, outFormat      ; move format to 1st argument
-    mov rsi, rbx            ; move fib number as 2nd argument
-    call printf
-    pop rbx
-    pob rax
-    
+    push rax            ; push registers to stack
+    push rbx
+    mov rdi, outFormat  ; set 1st argument (Print Format)
+    mov rsi, rbx        ; set 2nd argument (Fib Number)
+    call printf         ; printf(outFormat, rbx)
+    pop rbx             ; restore registers from stack
+    pop rax
+    ret
+
+loopFib:
+    call printFib       ; print current Fib number
+    add rax, rbx        ; get the next number
+    xchg rax, rbx       ; swap values
+    cmp rbx, 10000		    ; do rbx - 10
+    js loopFib		    ; jump if result is <0
+    ret
+
 Exit:
     mov rax, 60
     mov rdi, 0
